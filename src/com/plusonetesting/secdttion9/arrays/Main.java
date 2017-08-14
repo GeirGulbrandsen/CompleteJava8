ef233d062f86018c09740b45b320d7d32b5ee69e
Geir Gulbrandsen <dobbelg@gmail.com>
2017-08-14 20:06:06 +0100

    Add simple array exercise

---
+++ src/com/plusonetesting/secdttion9/arrays/Main.java
@@ -0,0 +1,41 @@
+package com.plusonetesting.secdttion9.arrays;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] intArray = getIntegers(5);
+
+        System.out.println(getAverage(intArray));
+
+    }
+
+    private static int[] getIntegers(int number) {
+
+        Scanner intScanner = new Scanner(System.in);
+        int[] ints = new int[number];
+        int i;
+
+        System.out.println("Enter " + number + " numbers:\r" );
+
+        for (i=0; i<number; i++) {
+            ints[i] = intScanner.nextInt();
+        }
+        return ints;
+    }
+
+    private static double getAverage(int[] intArray) {
+        int sum = 0;
+
+        for (int i:
+             intArray) {
+            sum += i;
+        }
+
+        return (double) sum / (double) intArray.length;
+    }
+
+
+}
